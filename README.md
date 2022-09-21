3309cfc41340241e38a25682f463e90d281b57bd
BonjourYY <fqymtu@gmail.com>
2022-09-21 16:59:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-rgxjep
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-rgxjep)
---
+++ index.js
@@ -0,0 +1,43 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// 数组的解构赋值
+// let [firstName, , , lastName] = ['jack', 'fan', 'qin', 'yun'];
+// console.log(firstName, lastName);
+
+// 对象的解构赋值
+// let options = {
+//   size: {
+//     width: 100,
+//     height: 200,
+//   },
+//   items: ['Cake', 'Donut'],
+//   extra: true,
+// };
+
+// let {
+//   size: { width: w, height: h },
+//   items: [item1, item2],
+//   title: t = 'demo',
+// } = options;
+// console.log(w, h, t, item1, item2);
+
+// 函数的解构赋值
+function fn({
+  name: n = 'fanqinyang',
+  sex: s,
+  // items: [item1, item2, ...rest],
+}) {
+  console.log(n, s);
+}
+
+let options = {
+  name: 'yun',
+  items: ['xxx1', 'xxx2', 'xxx3', 'xxx4'],
+};
+
+fn({ items: [] });
