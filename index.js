9ec1e96003120a55a8ab26a706694e175d1237c1
BonjourYY <fqymtu@gmail.com>
2022-09-23 14:39:14 +0800

    Math 对象

--- index.js
+++ index.js
@@ -5,26 +5,22 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-// 模板字符串
-// 多行
-console.log(`123
-123123
-2342342
-3453425`);
+let billion = 0x1e1b0;
+console.log(billion);
+// let num = 123312;
+console.log((123312).toString(36));
 
-// 嵌入表达式
-let [a, b] = [5, 10];
-console.log(`a+b的结果是：${a + b}`);
+// 关于舍入的方法
+let num = -1.5;
+// 向下舍入
+console.log(Math.floor(num));
 
-// 带标签的模板字符串
-let name = `JackFan`;
-let age = 24;
+// 向上舍入
+console.log(Math.ceil(num));
 
-function tag(strings, ...rest) {
-  console.log(rest);
-  return `${strings[0]}
-${rest}
-`;
-}
+// 四舍五入
+// 恰好等于0.5时，往正无穷（+∞）方向上舍入
+console.log(Math.round(num));
 
-console.log(tag`\u00A9${name}${age}and`);
+// 直接去除
+console.log(Math.trunc(num));
