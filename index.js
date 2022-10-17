fe2da0d5c0ebe456f2edd613499103143ed45c25
BonjourYY <fqymtu@gmail.com>
2022-10-17 15:09:26 +0800

    对象转换为原始值

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-// 为什么 0.1+0.2!=0.3 因为 精度缺失
-let result = Number((0.1).toString(2)) + Number((0.2).toString(2));
-console.log(result);
-// console.log(finalResult);
-// let finalResult = 0b0.30000000000000004
-console.log((9999999999999999).toString(2));
-console.log(0b100011100001101111001001101111110000010000000000000000);
-console.log(0.1 + 0.2);
+let user = {
+  name: 'fanqinyang',
+  toString() {
+    return '全能转换';
+  },
+};
+
+console.log(user--);
